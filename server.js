6bd4f6efcd8684b76e06328050c9715ef56e319a
Kong <90138804+ItsKong@users.noreply.github.com>
2024-09-10 11:37:58 +0700

    SF221 backend

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require('express');
+const route = require('../routes/apiRoutes')
+const app = express();
+const Users = require("../models/user");
+const connectDB = require('./dbconfig');
+// const insertRandomUsers = require('../models/insertRandomUsers')
+
+//tell express to use json
+app.use(express.json());
+//connect database
+connectDB();
+
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await Users.find();
+        // console.log(users);
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+
+app.get('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const user = await Users.findOne({ _id: userId});
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+app.use("/api", route);
+
+app.listen(process.env.SERVER_PORT || 3000, () => {
+    console.log("Server connected...");
+});
