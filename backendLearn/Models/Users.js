ea6c44046a9da66ad73c86191b8979bc4f9724c6
Theerapat <168794803+fay2220@users.noreply.github.com>
2024-09-13 15:53:25 +0700

    Delete backendLearn directory

--- backendLearn/Models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = mongoose.Schema({
-    StudentID : String,
-    Password :{
-        type : String
-    }
-},{timestamps : true});
-
-module.exports = mongoose.model('users',userSchema);
